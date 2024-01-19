3087de9cffcdbc47986cdbf01cae371c10bb13d4
agamkamran02 <agamkamran@gmail.com>
2024-01-18 16:37:01 -0800

    Added basic lint setup, see comment

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+import express from 'express'
+// let @agam know if import starts to work
 
-const app = express();
-const port = 8080; // You can choose any port
+const app = express()
+const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Back End:  "Meow Meow Brother."');
-});
+  res.send('Back End:  "Meow Meow Brother."')
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
+})
