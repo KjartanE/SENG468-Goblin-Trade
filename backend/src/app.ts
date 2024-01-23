f540b13b31829b606c4f1f2b003908c127061737
agamkamran02 <agamkamran@gmail.com>
2024-01-23 10:58:13 -0800

    fixed import in eslint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,12 @@
 import express from 'express'
-// let @agam know if import starts to work
 
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Back End:  "Meow Meow Brother. as you can see, I am a cat. :3"');
-});
+  res.send('Back End:  "Meow Meow Brother. as you can see, I am a cat. :3"')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`)
 })
