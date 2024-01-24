53cbf20c7a3561554cbe31c777a34580d04b338b
agamkamran02 <agamkamran@gmail.com>
2024-01-23 21:40:15 -0800

    Setup prettier on both FE and BE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"https://www.wargamer.com/wp-content/sites/wargamer/2022/11/dnd-goblin-5e-race-guide-goblin-face-close-up.jpg"} className="App-logo" alt="logo" />
-        <p>
-         GOBLIN
-        </p>
+        <img
+          src={
+            'https://www.wargamer.com/wp-content/sites/wargamer/2022/11/dnd-goblin-5e-race-guide-goblin-face-close-up.jpg'
+          }
+          className="App-logo"
+          alt="logo"
+        />
+        <p>GOBLIN</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
