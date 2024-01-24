1d0325f468dd4b64cfa7434edd898e0f1ff59334
agamkamran02 <agamkamran@gmail.com>
2024-01-23 21:01:15 -0800

    lint working on FE

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<App />);
+} else {
+    console.error('Root element not found');
+}
