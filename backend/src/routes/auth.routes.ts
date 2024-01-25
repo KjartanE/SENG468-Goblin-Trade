c347cc4c6c8bba5a90d21fe204f16b254f549959
CrippsBen <pumpkinflip86762@gmail.com>
2024-01-24 18:13:26 -0800

    login route added

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {AuthController} from '../controllers/auth.controller';
+
+const router = express.Router();
+const authController: AuthController = new AuthController();
+
+router.use(bodyParser.json());
+
+/**
+ * Login user
+ * (./auth/login)
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const login = async (req, res) => {
+    try {
+      //Validate request
+      if (!req.body.user_name || !req.body.password) {
+        res.status(400).send({ success: 'false', data: null, error: 'Feilds cannot be empty' });
+        return;
+      }
+  
+      // ask big K about this line from the capstone proj vvv
+      //const { user_name, password }: Userlogin = req.body;
+  
+      const response = await authController.login(req.body.user_name, req.body.password);
+      res.status(200).send(response);
+    } catch (err) {
+      res.status(401).send({ message: err });
+    }
+  };
+  router.post('/login', login);
+  
