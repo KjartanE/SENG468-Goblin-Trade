b95dd65d3a4b861f431e2c45a6101fe7fd5a7ada
Chaosfries <chaosfries45@gmail.com>
2024-01-24 17:19:56 -0800

    Implemented Routing, barebone Home and Login

---
+++ frontend/src/Pages/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <img
+          src={
+            'https://www.wargamer.com/wp-content/sites/wargamer/2022/11/dnd-goblin-5e-race-guide-goblin-face-close-up.jpg'
+          }
+          className="App-logo"
+          alt="logo"
+        />
+        <p>GOBLIN</p>
+      </header>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/Pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function Login() {
+  return (
+    <div className="Login">
+      <header className="App-header">
+        <p>Login Page</p>
+      </header>
+    </div>
+  )
+}
+
+export default Login
