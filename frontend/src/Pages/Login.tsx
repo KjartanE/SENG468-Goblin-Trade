23ec3522c96faf34d76d1d58174400cbfe53aac2
Chaosfries <chaosfries45@gmail.com>
2024-01-28 20:02:11 -0800

    Added MUI, starting Login page

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './App.css'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
 function Login() {
   return (
     <div className="Login">
       <header className="App-header">
         <p>Login Page</p>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button variant="contained">Contained</Button>
       </header>
     </div>
   )
