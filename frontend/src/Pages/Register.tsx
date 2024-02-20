cde49ac9d7a6e672cf359cdecea04a1a0252e26e
Chaosfries <chaosfries45@gmail.com>
2024-02-19 20:56:56 -0800

    adjusted fields and buttons, not hooked up yet

--- frontend/src/Pages/Register.tsx
+++ frontend/src/Pages/Register.tsx
@@ -9,12 +9,50 @@ import Typography from '@mui/material/Typography' // Import Typography for the e
 import { useAuth } from '../contexts/AuthContext'
 import { Container } from '@mui/material'
 
-export const loginSchema = Yup.object().shape({
+export const registerSchema = Yup.object().shape({
+  name: Yup.string().required('Required'),
   username: Yup.string().required('Required'),
   password: Yup.string().required('Required').min(3, 'Too Short!'),
 })
 
 function Register() {
+  const authContext = useAuth()
+  const navigate = useNavigate()
+  const [loginFailed, setLoginFailed] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const handleregister = async (registerDetails: {
+    name: string
+    username: string
+    password: string
+  }) => {
+    try {
+      type userRegister = {
+        username: string
+        email: string
+        password1: string
+        password2: string
+      }
+
+      const userRegisterData: userRegister = {
+        username: registerDetails.username,
+        email: registerDetails.name,
+        password1: registerDetails.password,
+        password2: registerDetails.password,
+      }
+
+      const user = await authContext.register(userRegisterData)
+
+      if (user) {
+        navigate('/login')
+      }
+    } catch (error) {
+      console.log(error)
+      setLoginFailed(true)
+      setErrorMessage('* Invalid credentials')
+    }
+  }
+
   return (
     <Container maxWidth="xl">
       <Box
@@ -31,23 +69,26 @@ function Register() {
       >
         <Box
           sx={{
-            width: '50%', // Set the width to 60%
-            margin: 'auto', // Center the box horizontally
-            backgroundColor: 'rgba(0, 0, 0, 0.5)', // Add a semi-transparent white background
-            padding: '20px', // Add some padding
-            borderRadius: '10px', // Add border radius for a rounded look
-            alignSelf: 'flex-end', // Align the box to the right
+            width: '50%',
+            margin: 'auto',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            padding: '20px',
+            borderRadius: '10px',
           }}
         >
           <p>Registeration Page</p>
           <Formik
-            initialValues={{ username: '', password: '' }}
-            validationSchema={loginSchema}
-            onSubmit={(values, { setSubmitting }) => {
-              // You can add your login logic here
-              console.log('Submitting:', values)
-              setSubmitting(false) // For demonstration, setSubmitting to false
+            initialValues={{
+              name: '',
+              username: '',
+              password: '',
+              confirmPassword: '',
             }}
+            onSubmit={(values, { setSubmitting }) => {
+              console.log('Submitting:', values)
+              setSubmitting(false)
+            }}
+            validationSchema={registerSchema}
           >
             {({
               values,
@@ -59,6 +100,22 @@ function Register() {
               isSubmitting,
             }) => (
               <form onSubmit={handleSubmit}>
+                <Box m={2}>
+                  <TextField
+                    fullWidth
+                    helperText={errors.name && touched.name && errors.name}
+                    id="Name"
+                    label="Name"
+                    name="name"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    placeholder="Enter a Display name"
+                    type="displayname"
+                    value={values.name}
+                    variant="outlined"
+                    margin="normal"
+                  />
+                </Box>
                 <Box m={2}>
                   <TextField
                     fullWidth
@@ -95,8 +152,27 @@ function Register() {
                   />
                 </Box>
                 <Box m={2}>
-                  <Button
+                  <TextField
                     fullWidth
+                    helperText={
+                      errors.password && touched.password && errors.password
+                    }
+                    label="Re-enter Password"
+                    name="confirmPassword"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    placeholder="Enter password again"
+                    type="password"
+                    value={values.confirmPassword}
+                    variant="outlined"
+                    margin="normal"
+                  />
+                </Box>
+                <Box m={2}>
+                  <Button
+                    sx={{
+                      width: '50%',
+                    }}
                     size="large"
                     type="submit"
                     variant="contained"
