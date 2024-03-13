e036371efd03aa229e3c0bc57acea8763f5bc2e7
CrippsBen <pumpkinflip86762@gmail.com>
2024-03-12 20:04:41 -0700

    include dialog file

--- frontend/src/components/StockTransactionsComponent.tsx
+++ frontend/src/components/StockTransactionsComponent.tsx
@@ -2,74 +2,93 @@ import { useStockTransactions } from '../contexts/StockTransactionsContext'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import { DataGrid, GridColDef, GridToolbar, GridRenderCellParams } from '@mui/x-data-grid'
+import ConfirmationDialogue from './ConfirmationDialogue'
+import React, { useState } from 'react'
 
-const columns: GridColDef[] = [
-  {
-    field: 'stock_tx_id',
-    headerName: 'Stock Transaction ID',
-    width: 200,
-  },
-  {
-    field: 'wallet_tx_id',
-    headerName: 'Wallet Transaction ID',
-    width: 200,
-  },
-  {
-    field: 'stock_id',
-    headerName: 'Stock ID',
-    width: 100,
-  },
-  {
-    field: 'order_status',
-    headerName: 'Order Status',
-    width: 150,
-  },
-  {
-    field: 'is_buy',
-    headerName: 'Is Buy',
-    width: 100,
-  },
-  {
-    field: 'order_type',
-    headerName: 'Order Type',
-    width: 100,
-  },
-  {
-    field: 'stock_price',
-    headerName: 'Stock Price',
-    width: 150,
-  },
-  {
-    field: 'quantity',
-    headerName: 'Quantity',
-    width: 150,
-  },
-  {
-    field: 'time_stamp',
-    headerName: 'Time Stamp',
-    width: 300,
-  },
-  {
-    field: 'cancel_order',
-    headerName: 'Cancel Order',
-    renderCell: (params: GridRenderCellParams<any, Date>) => (
-      <strong>
-        <Button
-          variant="contained"
-          size="small"
-          style={{ marginLeft: 16 }}
-          tabIndex={params.hasFocus ? 0 : -1}
-        >
-          Cancel
-        </Button>
-      </strong>
-    ),
-  },
-]
 
 function StockTransactionsComponent() {
   // Fetch stock portfolio from backend
   const { stock_transactions } = useStockTransactions()
+
+  const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false)
+
+  const columns: GridColDef[] = [
+    {
+      field: 'stock_tx_id',
+      headerName: 'Stock Transaction ID',
+      width: 200,
+    },
+    {
+      field: 'wallet_tx_id',
+      headerName: 'Wallet Transaction ID',
+      width: 200,
+    },
+    {
+      field: 'stock_id',
+      headerName: 'Stock ID',
+      width: 100,
+    },
+    {
+      field: 'order_status',
+      headerName: 'Order Status',
+      width: 150,
+    },
+    {
+      field: 'is_buy',
+      headerName: 'Is Buy',
+      width: 100,
+    },
+    {
+      field: 'order_type',
+      headerName: 'Order Type',
+      width: 100,
+    },
+    {
+      field: 'stock_price',
+      headerName: 'Stock Price',
+      width: 150,
+    },
+    {
+      field: 'quantity',
+      headerName: 'Quantity',
+      width: 150,
+    },
+    {
+      field: 'time_stamp',
+      headerName: 'Time Stamp',
+      width: 300,
+    },
+    {
+      field: 'cancel_order',
+      headerName: 'Cancel Order',
+      width: 150,
+      renderCell: (params: GridRenderCellParams<any, Date>) => (
+        <strong>
+          <Button
+            variant="contained"
+            size="small"
+            style={{ marginLeft: 16 }}
+            tabIndex={params.hasFocus ? 0 : -1}
+            onClick={() => {
+              setConfirmationDialogOpen(true);
+            }}
+          >
+            Cancel
+          </Button>
+          <ConfirmationDialogue
+            title="Cancel Order"
+            open={confirmationDialogOpen}
+            setOpen={setConfirmationDialogOpen}
+            onConfirm={() => {
+              // Cancel order
+            }}
+          >
+            Are you sure you want to cancel this order?
+          </ConfirmationDialogue>
+        </strong>
+      ),
+    },
+  ]
 
   if (stock_transactions == null) {
     return <Box>Loading...</Box>
