9a73bcdc4b4042cf62844f33056ca8f9d784d8a3
Chaosfries <chaosfries45@gmail.com>
2024-03-14 16:07:40 -0700

    Fixed Sidebar to close after clicking an option

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -8,14 +8,21 @@ interface SidebarProps {
     open: boolean
   ) => (event: React.KeyboardEvent | React.MouseEvent) => void
   isAuthenticated: boolean
+  setIsDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const Sidebar: React.FC<SidebarProps> = ({
   isOpen,
   toggleDrawer,
   isAuthenticated,
+  setIsDrawerOpen,
 }) => {
   const navigate = useNavigate()
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+    setIsDrawerOpen(false); 
+  };
 
   return (
     <Drawer anchor={'left'} open={isOpen} onClose={toggleDrawer(false)}>
@@ -23,20 +30,20 @@ const Sidebar: React.FC<SidebarProps> = ({
         {isAuthenticated ? (
           // Authenticated options
           <>
-            <ListItem button onClick={() => navigate('/')}>
+            <ListItem button onClick={() => handleNavigation('/')}>
               <ListItemText primary="Home" />
             </ListItem>
-            <ListItem button onClick={() => navigate('/user')}>
+            <ListItem button onClick={() => handleNavigation('/user')}>
               <ListItemText primary="Profile" />
             </ListItem>
           </>
         ) : (
           // Not authenticated options
           <>
-            <ListItem button onClick={() => navigate('/Login')}>
+            <ListItem button onClick={() => handleNavigation('/Login')}>
               <ListItemText primary="Login" />
             </ListItem>
-            <ListItem button onClick={() => navigate('/register')}>
+            <ListItem button onClick={() => handleNavigation('/register')}>
               <ListItemText primary="Register" />
             </ListItem>
           </>
